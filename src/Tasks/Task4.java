23bd07cff98a1d33594562ec9f60415d8bee952f
Zhaishylyk Rakhat <76480155+rahat-limit@users.noreply.github.com>
2023-04-04 10:21:57 +0600

    Update Task4.java

--- src/Tasks/Task4.java
+++ src/Tasks/Task4.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 public class Task4 {
   public static String task() {
     Scanner scan = new Scanner(System.in);
+//  @scan - get user input   
     int n = scan.nextInt();
+//  @n get factorial number
+    
     return "" + factorial(n);
   }
   public static int factorial(int n) {
-    if (n <= 1){
+    if (n <= 1) {
       return 1;
     }
+//  condition get rid of possible errors
     return n * factorial(n - 1);
   }
 }
