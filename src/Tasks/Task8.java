028d65470d6741de3a216d065f2960d1279fdafd
Zhaishylyk Rakhat <76480155+rahat-limit@users.noreply.github.com>
2023-04-04 09:41:20 +0600

    Update Task8.java

--- src/Tasks/Task8.java
+++ src/Tasks/Task8.java
@@ -2,11 +2,16 @@ package Tasks;
 import java.util.Scanner;
 
 public class Task8 {
-  public static String loop(String letter) {
-    return recFunction(letter.split(""), 0, letter.length());
+  public static String loop(String string) {
+    String[] list = string.split("");
+//  @list - list of letters   
+    int len = string.length();
+//  @len - number of list elements    
+    return recFunction(list, 0, len);
   }
   static String recFunction(String[] arr, int pos,  int stop) {
     if (pos < stop) {
+//    condition to stop recursion   
       if (isNum(arr[pos])) {
         return recFunction(arr, ++pos, stop);
       } else {
@@ -19,13 +24,16 @@ public class Task8 {
     try {
       Integer.parseInt(s);
       return true;
+//    Condition check is number format   
     } catch (NumberFormatException e) {
       return false;
     }
   }
   public static String task() {
     Scanner scan = new Scanner(System.in);
+//  @scan - get user input   
     String s = scan.nextLine();
+//  @scan - get line
     return loop(s);
   }
 }
