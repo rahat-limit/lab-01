f2fa98f97a3e201866cb27d023e30c63da4fb7e6
Zhaishylyk Rakhat <76480155+rahat-limit@users.noreply.github.com>
2023-04-04 09:43:50 +0600

    Update Task7.java

--- src/Tasks/Task7.java
+++ src/Tasks/Task7.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 public class Task7 {
   public static String task() {
     Scanner scan = new Scanner(System.in);
+//  @scan - get user input   
     int n = scan.nextInt();
+//  @n - length of list
     int[] arr = new int[n];
+//  @n - list of integers
     for (int i = 0; i < n; i++) {
       arr[i] = scan.nextInt();
     }
@@ -14,10 +17,12 @@ public class Task7 {
   }
   public static String reverse(int n, int[] arr) {
     if (n == arr.length) {
+//    condition to stop recursion
       return "";
     } else {
       reverse(n + 1, arr);
       System.out.print(arr[n] + " ");
+//    print each integer of reversed list
       return "";
     }
   }
