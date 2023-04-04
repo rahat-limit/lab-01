4978a45401838183aae409677ebac9e8b14ede5a
Zhaishylyk Rakhat <76480155+rahat-limit@users.noreply.github.com>
2023-04-04 12:04:13 +0600

    Update Task2.java

--- src/Tasks/Task2.java
+++ src/Tasks/Task2.java
@@ -1,15 +1,26 @@
 package Tasks;
 
 import java.util.Scanner;
+/**
+* Function task get input parameters.
+* @param arr is an array of received digits of which you need to find minimum digit.
+* @param n is the length of an array of digits.
+* @return this function returns minimum of given numbers.
+*/
+/**
+* Function findAverage that returns average of array.
+* @param arr is an array of received digits of which you need to find minimum digit.
+* @param n is the length of an array of digits.
+*/
 
 public class Task2 {
   public static String task() {
     Scanner scan = new Scanner(System.in);
-//  @scan - get input data
+//  @param scan - get input data
     int n = scan.nextInt();
-//  @n - length of list
+//  @param n - length of list
     int[] array = new int[n];
-//  @array - list of integers   
+//  @param array - list of integers   
     for (int i = 0; i < n; i++) {
       array[i] = scan.nextInt();
     }
@@ -21,8 +32,8 @@ public class Task2 {
 //      condition to last element, if last find average
         return (double)array[n - 1] / (double)array.length;
     } else if (n <= 0) {
-      return 0;
 //      condition due to avoid errors  
+        return 0;
     } else {
   //    find average of each element
         double average = (double)array[n - 1] / (double)array.length;
