1f018b8b8a1fccf8403ad911407a8adc7de0fd25
Zhaishylyk Rakhat <76480155+rahat-limit@users.noreply.github.com>
2023-04-04 09:24:33 +0600

    Update Task10.java

--- src/Tasks/Task10.java
+++ src/Tasks/Task10.java
@@ -4,11 +4,15 @@ import java.util.Scanner;
 public class Task10 {
   public static int euclidGCDAlgoReq(int a, int b) {
     if (b == 0) return a;
+//  return initial value if b == 0
     return euclidGCDAlgoReq(b, a % b);
+//  find great common divisor by recursion
   }
   public static String task() {
     Scanner scan = new Scanner(System.in);
+//  scan - get user input
     int[] array = new int[2];
+//  array - list of 2 integers
     for (int i = 0; i < 2; i++) {
       array[i] = scan.nextInt();
     }
