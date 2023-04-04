b1cd5beed974432d482ae1d4455786899be05ff7
Zhaishylyk Rakhat <76480155+rahat-limit@users.noreply.github.com>
2023-04-04 10:34:18 +0600

    Update Task3.java

--- src/Tasks/Task3.java
+++ src/Tasks/Task3.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Task3 {
   public static String task() {
     Scanner scan = new Scanner(System.in);
+//  @scan - get user input
     int n = scan.nextInt();
+//  n - get input integer
     if (isPrime(n, 2)) {
       return "Prime";
     } else {
@@ -15,14 +17,17 @@ public class Task3 {
   public static boolean isPrime(int n, int d) {
     boolean flag = true;
     if (n <= 1 || d < 2) {
+//    condition to determine composite integers
       flag = false;
       return flag;
     }
     if (n % d == 0 && n != d) {
+//    condition to determine composite integers
       flag = false;
       return flag;
     }
     if (d == n) {
+//    end of recursion   
       return flag;
     }
     return isPrime(n, d + 1);
