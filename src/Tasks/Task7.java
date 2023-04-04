29ac0547ca5fc8ffcd96a37c26bf53b99b0da414
Zhaishylyk Rakhat <76480155+rahat-limit@users.noreply.github.com>
2023-04-04 12:28:46 +0600

    Update Task7.java

--- src/Tasks/Task7.java
+++ src/Tasks/Task7.java
@@ -1,14 +1,22 @@
 package Tasks;
 import java.util.Scanner;
+/**
+* Function task get input parameters.
+* @param n is input number.
+* @param arr is an arr of input digits.
+* @return this function returns reversed list of numbers.
+*/
+/**
+* Function reverse that returns reversed list of numbers.
+* @param arr is an arr of input digits.
+* @param n is input number
+*/
 
 public class Task7 {
   public static String task() {
     Scanner scan = new Scanner(System.in);
-//  @scan - get user input   
     int n = scan.nextInt();
-//  @n - length of list
     int[] arr = new int[n];
-//  @n - list of integers
     for (int i = 0; i < n; i++) {
       arr[i] = scan.nextInt();
     }
