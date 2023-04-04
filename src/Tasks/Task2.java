b75ca5c91ed162e0b04c1a4e01e3af0d78531bfb
Zhaishylyk Rakhat <76480155+rahat-limit@users.noreply.github.com>
2023-04-04 10:46:20 +0600

    Update Task2.java

--- src/Tasks/Task2.java
+++ src/Tasks/Task2.java
@@ -5,8 +5,11 @@ import java.util.Scanner;
 public class Task2 {
   public static String task() {
     Scanner scan = new Scanner(System.in);
+//  @scan - get input data
     int n = scan.nextInt();
+//  @n - length of list
     int[] array = new int[n];
+//  @array - list of integers   
     for (int i = 0; i < n; i++) {
       array[i] = scan.nextInt();
     }
@@ -15,13 +18,16 @@ public class Task2 {
   }
   public static double findAverage(int[] array, int n) {
     if (n == 1) {
-      return (double)array[n - 1] / (double)array.length;
+//      condition to last element, if last find average
+        return (double)array[n - 1] / (double)array.length;
     } else if (n <= 0) {
       return 0;
+//      condition due to avoid errors  
     } else {
-      double average = (double)array[n - 1] / (double)array.length;
-      double d = findAverage(array, n - 1);
-      return Math.round(average + d);
+  //    find average of each element
+        double average = (double)array[n - 1] / (double)array.length;
+        double d = findAverage(array, n - 1);
+        return Math.round(average + d);
     }
   }
 }
