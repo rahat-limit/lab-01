024a027ce0127031ecdf8527c13c38e48ae67490
Zhaishylyk Rakhat <76480155+rahat-limit@users.noreply.github.com>
2023-04-04 10:06:21 +0600

    Update Task5.java

--- src/Tasks/Task5.java
+++ src/Tasks/Task5.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 public class Task5 {
   public static String task() {
     Scanner scan = new Scanner(System.in);
+//  @scan - get user input   
     int n = scan.nextInt();
+//  @n - input integer
     int[] array = new int[n + 1];
+//  @array - list of fibonacci sequence using recursion
     for (int i = 0; i < n + 1; i++) {
       array[i] = fibonacci(i);
     }
@@ -17,6 +20,7 @@ public class Task5 {
     } else if (n == 1) {
       return 1;
     } else {
+//    conditions due to avoid possible errors
       return fibonacci(n - 1) + fibonacci(n - 2);
     }
   }
